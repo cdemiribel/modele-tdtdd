33e8e03b8f8591d915de7b3350731d8c0911a529
de Miribel Clément <clementdemiribel@MacBook-Air-de-de.local>
2022-10-14 14:54:14 +0200

    fin partie translation

--- tddstart/app/src/main/java/tddstart/Dictionary.java
+++ tddstart/app/src/main/java/tddstart/Dictionary.java
@@ -1,15 +1,32 @@
 package tddstart;
 
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 public class Dictionary {
     private String name;
-    public Dictionary(String name){
+    private Map<String,String> hasmap; 
+    public Dictionary(String name, Map hasmap){
         this.name=name;
+        this.hasmap= new HashMap<>();
     }
     public String getName() {
         return name;
     }
     public boolean IsEmpty(){
             return false;
+    }
+    
+    public void addTranslation(String a, String b){
+        hasmap.put(a,b);
+
+    }
+
+    public String getTranslation(String b){
+        if(hasmap.containsKey(b))
+            return hasmap.get(b);
+        return  "";
     }
     
 }
--- tddstart/app/src/test/java/tddstart/DictionaryTest.java
+++ tddstart/app/src/test/java/tddstart/DictionaryTest.java
@@ -2,18 +2,29 @@ package tddstart;
 
 import org.junit.Test;
 import static org.junit.Assert.*;
+
+import java.util.Map;
+
 import static org.hamcrest.CoreMatchers.equalTo;
 
 public class DictionaryTest {
+    private Map<String,String> m;
     @Test public void testDictionaryName() {
-        Dictionary dict = new Dictionary("Example"); 
+        Dictionary dict = new Dictionary("Example",m); 
 
         assertThat(dict.getName(), equalTo("Example"));
         }
     
     @Test public void testDictionnary(){
-        Dictionary d = new Dictionary(null);
+        Dictionary d = new Dictionary(null,m);
 
         assertThat(d.IsEmpty(), equalTo(false));
+    }
+    @Test public void testOneTranslation() {
+        Dictionary d = new Dictionary("dic",m);
+        d.addTranslation("contre", "against");
+        assertThat(d.getTranslation("contre"), equalTo("against"));
+        d.addTranslation("paix", "peace");
+        assertThat(d.getTranslation("paix"), equalTo("peace"));
     }
 }
